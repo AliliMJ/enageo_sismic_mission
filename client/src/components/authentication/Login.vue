b49f2e03c5e2787a93c1cae8ec7db6792ebb2aa6
alili <alili.mohamedlamine@gmail.com>
2023-04-02 16:00:52 +0100

    merge login

--- client/src/components/authentication/Login.vue
+++ client/src/components/authentication/Login.vue
@@ -10,6 +10,7 @@ const router = useRouter();
 
 const message = useMessage();
 
+const showModal = ref(false);
 const login = async (e) => {
   e.preventDefault();
   try {
@@ -24,39 +25,82 @@ const login = async (e) => {
 
 <template>
   <div class="main">
-    <div class="container">
+    <div class="container" v-show="showModal">
+      <h1 class="authLabel">creer un nouveau compte</h1>
       <form>
-        <h1 class="authLabel">Authentification</h1>
-        <img class="logoImg" src="@/assets/ENAGEO.png" alt="erreur" />
         <div class="inputContainer">
-          <input
-            v-model="email"
-            class="input"
-            type="text"
-            placeholder=" "
-            required
-          />
-          <label for="username" class="placeholder">nom d'utilisateur</label>
+          <input class="input" type="text" placeholder=" " required />
+          <label for="email" class="placeholder">Adresse email</label>
         </div>
-        <div class="separator"></div>
+
         <div class="inputContainer">
-          <input
-            v-model="password"
-            class="input"
-            type="password"
-            placeholder=" "
-            required
-          />
+          <input class="input" type="password" placeholder=" " required />
           <label for="password" class="placeholder">mot de passe</label>
         </div>
 
-        <div class="forget">mot de passe oubliee</div>
+        <div class="inputContainer">
+          <input class="input" type="password" placeholder=" " required />
+          <label for="password" class="placeholder">nom</label>
+        </div>
 
-        <button class="button-4" @click="login">connexion</button>
+        <div class="inputContainer">
+          <input class="input" type="password" placeholder=" " required />
+          <label for="password" class="placeholder">prenom</label>
+        </div>
 
-        <div class="create">creer un compte</div>
+        <div class="inputContainer">
+          <input class="input" type="password" placeholder=" " required />
+          <label for="password" class="placeholder">mot de passe</label>
+        </div>
+
+        <button class="button-4" value="creer" />
       </form>
+
+      <hr class="hrSeparator" />
+      <div class="create" v-on:click="showModal = !showModal">
+        retour a la page de connexion
+      </div>
     </div>
+
+    <transition>
+      <div class="container" v-show="!showModal">
+        <h1 class="authLabel">Authentification</h1>
+        <img class="logoImg" src="@/assets/ENAGEO.png" alt="erreur" />
+        <form>
+          <div class="inputContainer">
+            <input
+              class="input"
+              v-model="email"
+              type="text"
+              placeholder=" "
+              required
+            />
+            <label for="username" class="placeholder">email</label>
+          </div>
+          <div class="inputContainer">
+            <input
+              class="input"
+              type="password"
+              v-model="password"
+              placeholder=" "
+              required
+            />
+            <label for="password" class="placeholder">mot de passe</label>
+          </div>
+
+          <div class="forgetContainer">
+            <label class="forget">mot de passe oubliée ?</label>
+          </div>
+
+          <button class="button-4">Connexion</button>
+        </form>
+
+        <hr class="hrSeparator" />
+        <div class="create" v-on:click="showModal = !showModal">
+          créer un compte
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -67,8 +111,22 @@ const login = async (e) => {
   margin: 0;
   padding: 0;
   background-color: #ffed00;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
   overflow: hidden;
+}
+
+.container1 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: red;
+  width: 500px;
+  height: 500px;
+  border-radius: 10px;
+  padding: 15px;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+  text-align: center;
 }
 
 .container {
@@ -77,11 +135,12 @@ const login = async (e) => {
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: rgb(255, 255, 255);
-  width: 400px;
-  height: 450px;
+  width: 500px;
+  height: 500px;
   border-radius: 10px;
-  padding: 10px;
+  padding: 15px;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+  text-align: center;
 }
 
 .authLabel {
@@ -89,32 +148,45 @@ const login = async (e) => {
   text-align: center;
 }
 
+.forgetContainer {
+  margin: -15px 0px 30px 0px;
+}
+
 .forget {
-  margin: 0px 0px 10px 0px;
   padding: 0;
-  top: -10px;
-  left: 65px;
   text-decoration: underline;
+  position: relative;
+  left: 50px;
+}
+
+.create {
+  text-align: center;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.create:active {
+  color: #787878;
 }
 
 .logoImg {
-  width: 100px;
-  height: 100px;
-  left: 138px;
-  margin-bottom: 30px;
+  width: 130px;
+  height: 130px;
+  text-align: center;
+  position: relative;
 }
 
-.separator {
-  height: 15px;
+.hrSeparator {
+  margin: 30px 50px 10px 50px;
 }
 
 /* start input style */
 
 .inputContainer {
-  height: 50px;
   position: relative;
   width: 100%;
   text-align: center;
+  margin: 20px 0px 20px 0px;
 }
 
 .input {
@@ -124,34 +196,35 @@ const login = async (e) => {
   outline: 0;
   padding: 4px 20px 0;
   height: 40px;
+  /* border: 0.3px solid #202020; */
 }
 
 .input:focus {
-  border: 1px solid #fbff00;
+  border: 1px solid #35bc00;
 }
 
 .placeholder {
   color: #96969b;
   background-color: white;
   font-family: sans-serif;
-  left: 70px;
+  left: 125px;
   line-height: 14px;
   pointer-events: none;
   position: absolute;
   transform-origin: 0 50%;
   transition: ease 200ms, color 150ms;
-  top: 12px;
+  top: 13px;
 }
 
 .input:focus ~ .placeholder,
 .input:not(:placeholder-shown) ~ .placeholder {
   transform: translateY(-18px) translateX(10px);
-  color: #ffed00;
+  color: #35bc00;
   font-size: 14px;
 }
 
 .input:not(:placeholder-shown) ~ .placeholder {
-  color: #ffed00;
+  color: #35bc00;
 }
 
 /* end input style */
@@ -169,8 +242,7 @@ const login = async (e) => {
   color: #24292e;
   cursor: pointer;
   display: inline-block;
-  font-family: -apple-system, system-ui, 'Segoe UI', Helvetica, Arial,
-    sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji';
+  /* font-family: -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"; */
   font-size: 16px;
   font-weight: 500;
   line-height: 20px;
@@ -184,11 +256,9 @@ const login = async (e) => {
   vertical-align: middle;
   white-space: nowrap;
   word-wrap: break-word;
-  width: 250px;
+  width: 280px;
   height: 40px;
-  text-align: center;
   font-weight: bold;
-  left: 65px;
 }
 
 .button-4:hover {
@@ -198,20 +268,20 @@ const login = async (e) => {
 }
 
 .button-4:disabled {
-  background-color: #00ed14;
+  background-color: #e6ea02;
   border-color: rgba(27, 31, 35, 0.15);
-  color: #65f300;
+  color: #e6ea02;
   cursor: default;
 }
 
 .button-4:active {
-  background-color: #00ff11;
-  box-shadow: rgb(0, 228, 11) 0 1px 0 inset;
+  background-color: #e6ea02;
+  box-shadow: rgb(209, 228, 0) 0 1px 0 inset;
   transition: none 0s;
 }
 
 .button-4:focus {
-  outline: 1px green;
+  outline: 1px rgb(220, 232, 0);
 }
 
 .button-4:before {
@@ -223,4 +293,25 @@ const login = async (e) => {
 }
 
 /* End button style  */
+
+/* overlay animation start */
+.container1-enter-from {
+  opacity: 0;
+}
+.container1-enter-to {
+  opacity: 1;
+}
+.container1-enter-active {
+  transition: all 3s ease;
+}
+.container1-leave-from {
+  opacity: 1;
+}
+.container1-leave-to {
+  opacity: 0;
+}
+.container1-leave-active {
+  transition: all 3s ease;
+}
+/* overlay animation end */
 </style>
