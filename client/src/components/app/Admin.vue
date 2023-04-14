775197f1b6dbc894ed67fbd20754a27d2cc5499d
alili <alili.mohamedlamine@gmail.com>
2023-04-15 00:48:30 +0100

    J

--- client/src/components/app/Admin.vue
+++ client/src/components/app/Admin.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRouter } from 'vue-router';
-import { NLayout, NLayoutSider, NLayoutContent } from 'naive-ui';
+import { NLayout, NLayoutSider, NLayoutContent, NLayoutHeader } from 'naive-ui';
 
 import AdminMenu from '../menu/AdminMenu.vue';
 //const auth = useAuth();
@@ -9,15 +9,29 @@ const router = useRouter();
 </script>
 
 <template>
-  <NLayout has-sider>
-    <NLayoutSider>
-      <AdminMenu />
-    </NLayoutSider>
+  <NLayout>
+    <NLayoutHeader bordered> </NLayoutHeader>
 
-    <NLayoutContent>
-      <Suspense>
-        <RouterView />
-      </Suspense>
-    </NLayoutContent>
+    <NLayout has-sider>
+      <NLayoutSider bordered>
+        <AdminMenu />
+      </NLayoutSider>
+
+      <NLayoutContent>
+        <Suspense>
+          <RouterView />
+        </Suspense>
+      </NLayoutContent>
+    </NLayout>
   </NLayout>
 </template>
+
+<style scoped>
+.n-layout-sider {
+  height: 100vh;
+}
+
+.n-layout-header {
+  padding: 24px;
+}
+</style>
