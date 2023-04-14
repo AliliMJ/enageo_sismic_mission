782d413b402e65765201ea93583523aa294267ec
abdelkrim99 <kmioalg03@gmail.com>
2023-04-14 19:14:08 +0100

    c

--- client/src/components/tables/EmployesTable.vue
+++ client/src/components/tables/EmployesTable.vue
@@ -1,8 +1,9 @@
 <script setup>
-import axios from 'axios';
-<<<<<<< HEAD
-import { useAuth } from '../../stores/authentication';
-import STable from 'common/STable.vue';
+import axios from "axios";
+import { useAuth } from "../../stores/authentication";
+import STable from "common/STable.vue";
+import { NH1 } from "naive-ui";
+
 const auth = useAuth();
 
 console.log(auth.user.hashPassword);
@@ -13,57 +14,41 @@ const req = {
 
 console.log(req);
 
-const emps = (await axios.post('http://localhost:3000/employees', req)).data;
+const employes = (await axios.get("http://localhost:3000/employes")).data;
 
-const cols = [
-  { title: 'id', key: 'id' },
-  { title: 'status', key: 'status' },
-  { title: 'fonction', key: 'fonction' },
-=======
-
-import STable from 'common/STable.vue';
-import { NH1 } from 'naive-ui';
-
-const employes = (await axios.get('http://localhost:3000/employes')).data;
-
-const fonctions = (await axios.get('http://localhost:3000/employes/fonctions'))
+const fonctions = (await axios.get("http://localhost:3000/employes/fonctions"))
   .data;
 
 const cols = [
-  { title: 'id', key: 'id' },
-  { title: 'email', key: 'email' },
-  { title: 'nom', key: 'nom' },
-  { title: 'prénom', key: 'prenom' },
+  { title: "id", key: "id" },
+  { title: "email", key: "email" },
+  { title: "nom", key: "nom" },
+  { title: "prénom", key: "prenom" },
   {
-    title: 'fonction',
-    key: 'fonctionId',
+    title: "fonction",
+    key: "fonctionId",
     render(row) {
       return fonctions.filter((f) => f.id === row.fonctionId)[0].nom;
     },
   },
   {
-    title: 'Date naissance',
-    key: 'dateNaissance',
+    title: "Date naissance",
+    key: "dateNaissance",
     render(row) {
       return new Date(row.dateNaissance).toLocaleDateString();
     },
   },
   {
-    title: 'Date Adhésion',
-    key: 'dateAdhesion',
+    title: "Date Adhésion",
+    key: "dateAdhesion",
     render(row) {
       return new Date(row.dateAdhesion).toLocaleDateString();
     },
   },
->>>>>>> a026253fcc141808aa21ddf398dea0db5fd32bea
 ];
 </script>
 
 <template>
-<<<<<<< HEAD
-  <STable :data="emps" :columns="cols" />
-=======
   <NH1>Employés</NH1>
   <STable :data="employes" :columns="cols" />
->>>>>>> a026253fcc141808aa21ddf398dea0db5fd32bea
 </template>
