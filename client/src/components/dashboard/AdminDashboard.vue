dc7f9b6b4669f0141361b3005d4dbbf132d53427
abdelkrim99 <kmioalg03@gmail.com>
2023-05-13 20:21:59 +0100

    c

--- client/src/components/dashboard/AdminDashboard.vue
+++ client/src/components/dashboard/AdminDashboard.vue
@@ -350,7 +350,7 @@ const bar2Data = {
   background-color: white;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
   border-radius: 8px;
-  font-size: 16px;
+  font-size: 15px;
   font-weight: bold;
   overflow: hidden;
   padding: 12px 8px 10px 8px;
