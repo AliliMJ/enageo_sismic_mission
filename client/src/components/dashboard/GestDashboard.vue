198a12393de257d57d3f3cd56a870fd6b5c835c4
alili <alili.mohamedlamine@gmail.com>
2023-05-25 19:09:08 +0100

    The scooped meme

--- client/src/components/dashboard/GestDashboard.vue
+++ client/src/components/dashboard/GestDashboard.vue
@@ -452,7 +452,7 @@ onMounted(async () => {
 });
 </script>
 
-<style scooped>
+<style scoped>
 .type-pie {
   background-color: white;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
