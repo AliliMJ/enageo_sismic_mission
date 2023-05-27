30797684b3bcfe9fb3d9e3332fb879b20c877cf2
alili <alili.mohamedlamine@gmail.com>
2023-05-27 20:33:05 +0100

    restore directeur.vue

---
+++ client/src/components/app/Directeur.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {
+  NLayout,
+  NLayoutSider,
+  NLayoutContent,
+  NCard,
+  NScrollbar,
+} from 'naive-ui';
+import DirecteurMenu from '../menu/DirecteurMenu.vue';
+</script>
+
+<template>
+  <NLayout has-sider>
+    <NLayoutSider bordered>
+      <DirecteurMenu />
+    </NLayoutSider>
+
+    <NLayoutContent embedded>
+      <Suspense>
+        <NScrollbar style="height: 85vh">
+          <RouterView />
+        </NScrollbar>
+      </Suspense>
+    </NLayoutContent>
+  </NLayout>
+</template>
+
+<style scoped>
+.n-layout-sider {
+  height: 100vh;
+}
+.n-layout-content {
+  padding-left: 10px;
+  padding-top: 10px;
+}
+</style>
