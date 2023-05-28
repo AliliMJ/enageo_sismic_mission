c44b3e9e6788c5c86096df354faa7cc099ed26d6
alili <alili.mohamedlamine@gmail.com>
2023-05-28 13:47:01 +0100

    carte menu item

--- client/src/components/menu/DirecteurMenu.vue
+++ client/src/components/menu/DirecteurMenu.vue
@@ -9,7 +9,10 @@ import { useRoute } from 'vue-router';
 import { optionsCompte, optionDashboard } from './menuOptions.js';
 import { renderIcon, renderMenuItem } from '../../utils/render.js';
 import { Route } from '../../enums';
-import { AnalyticsOutline as Analytics } from '@vicons/ionicons5';
+import {
+  AnalyticsOutline as Analytics,
+  LocationOutline as Location,
+} from '@vicons/ionicons5';
 
 const route = useRoute();
 const selectedKey = ref(route.name);
@@ -23,6 +26,11 @@ const menuOptions = [
     title: renderMenuItem('Analyse', Route.Analyse),
     key: Route.Analyse,
     icon: renderIcon(Analytics),
+  },
+  {
+    title: renderMenuItem('Carte', Route.Carte),
+    key: Route.Carte,
+    icon: renderIcon(Location),
   },
 
   ...optionsCompte,
